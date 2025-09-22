648bb53c0fe81d4fd266a5b2589b459c7a6ef44f
cptamrgaber <61318505+cptamrgaber@users.noreply.github.com>
2025-09-22 16:42:46 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // تسكيت تحذير Turbopack (اختياري)
   turbopack: { root: __dirname },
-  // احذف/علّق السطور دي لو كانت موجودة:
-  // output: "export",
-  // trailingSlash: true,
-  // images: { unoptimized: true }
-};
-
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  
+   };
 export default nextConfig;
